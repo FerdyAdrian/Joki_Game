ec5a1085f53e236987c494f8871eacfe824ca50d
Ferdi Adrian <hateme112211@gmail.com>
2025-06-29 17:47:34 +0700

    feat: tambah fitur show/hide password di form login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -29,7 +29,14 @@
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <div style="position: relative;">
+                                    <input id="password" type="password" class="form-control" name="password" required autocomplete="current-password" style="padding-right: 40px;">
+                                    <span onclick="togglePassword()" style="position: absolute; right: 10px; top: 50%; transform: translateY(-50%); cursor: pointer;">
+                                        <svg id="eyeIcon" xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="gray" viewBox="0 0 24 24">
+                                            <path d="M12 5c-7.633 0-12 7-12 7s4.367 7 12 7 12-7 12-7-4.367-7-12-7zm0 12c-2.761 0-5-2.239-5-5s2.239-5 5-5 5 2.239 5 5-2.239 5-5 5zm0-8c-1.654 0-3 1.346-3 3s1.346 3 3 3 3-1.346 3-3-1.346-3-3-3z"/>
+                                        </svg>
+                                    </span>
+                                </div>
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -76,4 +83,18 @@
         </div>
     </div>
 </div>
+
+<script>
+function togglePassword() {
+    var input = document.getElementById("password");
+    var icon = document.getElementById("eyeIcon");
+    if (input.type === "password") {
+        input.type = "text";
+        icon.innerHTML = '<path d="M12 5c-7.633 0-12 7-12 7s4.367 7 12 7 12-7 12-7-4.367-7-12-7zm0 12c-2.761 0-5-2.239-5-5s2.239-5 5-5 5 2.239 5 5-2.239 5-5 5zm0-8c-1.654 0-3 1.346-3 3s1.346 3 3 3 3-1.346 3-3-1.346-3-3-3z" fill="gray"/><line x1="1" y1="1" x2="23" y2="23" stroke="red" stroke-width="2"/>';
+    } else {
+        input.type = "password";
+        icon.innerHTML = '<path d="M12 5c-7.633 0-12 7-12 7s4.367 7 12 7 12-7 12-7-4.367-7-12-7zm0 12c-2.761 0-5-2.239-5-5s2.239-5 5-5 5 2.239 5 5-2.239 5-5 5zm0-8c-1.654 0-3 1.346-3 3s1.346 3 3 3 3-1.346 3-3-1.346-3-3-3z"/>';
+    }
+}
+</script>
 @endsection
